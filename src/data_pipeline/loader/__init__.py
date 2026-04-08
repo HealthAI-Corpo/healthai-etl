a24e245022a1cf0a30523f72edd2dd63f2ab98ba
LoricWorms <loricworms@orange.fr>
2026-04-08 17:10:57 +0200

    git commit -m "fix(etl): ruff format loader __init__"

--- src/data_pipeline/loader/__init__.py
+++ src/data_pipeline/loader/__init__.py
@@ -1,13 +1,13 @@
 from .db_writer import (
-	ingest_cleaned_data,
-	loader_pipeline,
-	mark_source_file_as_processed,
-	save_dataframe_to_csv,
+    ingest_cleaned_data,
+    loader_pipeline,
+    mark_source_file_as_processed,
+    save_dataframe_to_csv,
 )
 
 __all__ = [
-	"ingest_cleaned_data",
-	"loader_pipeline",
-	"mark_source_file_as_processed",
-	"save_dataframe_to_csv",
+    "ingest_cleaned_data",
+    "loader_pipeline",
+    "mark_source_file_as_processed",
+    "save_dataframe_to_csv",
 ]
