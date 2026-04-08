7dd3c0a6ca664cf5fd55b168e241b58f00dcd4fe
timeo <timeo.danois@epsi.fr>
2026-04-08 14:28:28 +0200

    pipeline auto v1 avec exercice

---
+++ src/data_pipeline/loader/__init__.py
@@ -0,0 +1,3 @@
+from .db_writer import ingest_cleaned_data, loader_pipeline, save_dataframe_to_csv
+
+__all__ = ["ingest_cleaned_data", "loader_pipeline", "save_dataframe_to_csv"]
--- src/main.py
+++ src/main.py
@@ -1,51 +1,11 @@
-from datetime import date
+from data_pipeline.pipeline import execute_pipeline_exercisedb_hobby
 from data_pipeline.database import SessionLocal, engine, Base
-from data_pipeline.models import Utilisateur, ProfilSante
-
-
-def run_test_insert():
-    session = SessionLocal()
-    try:
-        print("🔍 Vérification de l'utilisateur de test...")
-
-        user_email = "jordan.test@healthai.com"
-        existing_user = session.query(Utilisateur).filter_by(email=user_email).first()
-
-        if not existing_user:
-            #  Création de l'entité de l'utilisateur
-            new_user = Utilisateur(
-                nom="Nkunga",
-                prenom="Jordan",
-                email=user_email,
-                date_de_naissance=date(1995, 5, 20),
-                genre="Masculin",
-                mot_de_passe_hash="hash_securise_123",
-            )
-
-            #  Création profil santé lié à l'utilisateur
-            ProfilSante(
-                objectif_principal="Prise de masse",
-                poids_kg=75.5,
-                taille_cm=180,
-                imc=23.3,
-                utilisateur=new_user,  # Le lien est fait ici
-            )
-
-            session.add(new_user)  # Ajoute aussi le profil grâce à la relation
-            session.commit()
-            print(f" Utilisateur '{new_user.prenom}' et son Profil Santé créés !")
-        else:
-            print(f"L'utilisateur {user_email} existe déjà.")
-
-    except Exception as e:
-        session.rollback()
-        print(f"Erreur : {e}")
-    finally:
-        session.close()
-
 
 if __name__ == "__main__":
     print("♻️ Réinitialisation de la base de données...")
     Base.metadata.drop_all(bind=engine)
     Base.metadata.create_all(bind=engine)
-    run_test_insert()
+
+    print("Lancement de l'ETL")
+    test = execute_pipeline_exercisedb_hobby()
+    print("Fin de l'ETL")
