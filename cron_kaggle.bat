bf302a719ecf03573b00cba1b2ffede477effa66
Joeybadd <jnkunga@hotmail.com>
2026-04-11 11:50:12 +0200

    chore: universalisation des scripts de cron pour windows et unix closes #21

--- cron_kaggle.bat
+++ cron_kaggle.bat
@@ -1,6 +1,6 @@
 @echo off
-:: 1. Se déplacer dans le dossier du projet
-cd /d "C:\Users\jorda\Documents\Cours EPSI\MSPR\BLOC1\healthai-etl"
+:: 1. Se déplacer dynamiquement dans le dossier du script
+cd /d "%~dp0"
 
-:: 2. Lancer le script via uv (utilise le chemin trouvé à l'étape 1)
-"c:\Users\jorda\.local\bin\uv.exe" run python src/data_pipeline/downloader/api_client.py >> logs\cron_kaggle.log 2>&1
+:: 2. Lancer la commande Docker (qui elle est universelle)
+docker compose run --rm api uv run python src/data_pipeline/downloader/api_client.py >> logs\cron_kaggle.log 2>&1
